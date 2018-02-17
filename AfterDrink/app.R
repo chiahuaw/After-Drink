a32cb2667bb6197db58dcf59a06304347335fe38
chiahuaw <chiahua3@gmail.com>
2018-02-17 12:06:06 +0800

    shinyapp初版，年補貼成本估計完成。

--- AfterDrink/app.R
+++ AfterDrink/app.R
@@ -16,39 +16,13 @@ library(knitr)
 set.seed(12771)
 options(scipen = 99999999)
 
-load("taxi.RData")
+#load("taxi.RData")
+load("taxiTrip.RData")
 
 thm = function() {
   theme_gray(base_family = "黑體-繁 中黑") + theme(text=element_text(size=18))
 }
 
-# 從 Google Maps API 導航的資料裡，取得開車的部分。
-
-taxi.trip = data.frame()
-
-for (i in 1:length(taxi)) {
-  x1 = taxi[i][[1]]$routes[[1]]$legs[[1]]$start_location$lat[1]
-  x2 = taxi[i][[1]]$routes[[1]]$legs[[1]]$end_location$lat[1]
-  y1 = taxi[i][[1]]$routes[[1]]$legs[[1]]$start_location$lng[1]
-  y2 = taxi[i][[1]]$routes[[1]]$legs[[1]]$end_location$lng[1]
-  
-  if (taxi[i][[1]]$routes[[1]]$legs[[1]]$steps[[1]]$travel_mode != "DRIVING") {
-    next
-  }
-  
-  Temp = data.frame(i = i,
-                    d = distm(c(y1,x1),c(y2,x2),fun=distHaversine)[1],
-                    m = taxi[i][[1]]$routes[[1]]$legs[[1]]$distance$value[1],
-                    t = round(taxi[i][[1]]$routes[[1]]$legs[[1]]$duration$value[1]/60,2))
-  taxi.trip = rbind(taxi.trip,Temp,stringsAsFactors=F)
-  rm(x1);rm(y1);rm(x2);rm(y2);rm(Temp)
-  
-}
-
-
-
-taxi.trip = mutate(taxi.trip,fee = round((100+round(ifelse((m-1000)<0,0,m-1000)/250,0)*5),0),
-                   nightfee=round((100+round(ifelse((m-800)<0,0,m-800)/200,0)*5),0))
 
 
 
@@ -74,10 +48,10 @@ ui <- fluidPage(
                    "你覺得一天可能平均有多少車次：",value = 75),
          selectInput("plan","補貼策略：",choices = c("固定金額","固定比例"),multiple = F,selected = "固定比例"),
         numericInput("money",
-                   "補貼比例或金額：",value = 0.1),
-         selectInput("selfpay","有最小自付額嗎？",choices = c("是","否"),multiple = F,selected = "是"),
+                   "補貼比例或金額：",value = 0.5),
+         selectInput("selfpay","有最小自付額嗎？",choices = c("是","否"),multiple = F,selected = "否"),
         numericInput("selfpaymoney",
-                   "最小自付額（元）：",value = 100)
+                   "最小自付額（元）：",value = 0)
       ),
       
       # Show a plot of the generated distribution
@@ -89,9 +63,9 @@ ui <- fluidPage(
         plotOutput("distPlot"),
         textOutput("text1"),
         plotOutput("distPlot2"),
-         textOutput("text2")#,
-        # plotOutput("distPlot3"),
-        # textOutput("text3")
+         textOutput("text2"),
+        plotOutput("distPlot3"),
+        textOutput("text3")
       )
    )
 )
@@ -153,59 +127,84 @@ server <- function(input, output) {
      paste0("一年的計程車費成本可能落在", quantile(taxi.est.y$taxiFee[order(taxi.est.y$taxiFee)],0.1)[1],"元到", quantile(taxi.est.y$taxiFee[order(taxi.est.y$taxiFee)],0.9)[1],"元之間。（90%信賴區間）")
    })
    
-   # output$distPlot3 <- renderPlot({
-   #   
-   #   s.sd = abs(as.numeric(input$carsmax)-as.numeric(input$carsmin))/2
-   #   s.mean = as.numeric(input$carsmean)
-   #   # s.cars = rnorm(n = 365,mean = s.mean,sd=s.sd)
-   #   # s.cars = ifelse(s.cars<0,0,s.cars)
-   #   
-   #   if (input$plan == "固定金額") {
-   #     Temp1 = 1
-   #   } else {
-   #     Temp1 = 2
-   #   }
-   #   
-   #   Temp2 = as.numeric(input$money) 
-   #   
-   #   if (input$selfpay=="是") {
-   #     Temp3 = as.numeric(input$selfpaymoney)
-   #   } else {
-   #     Temp3 = 0
-   #   }
-   #   
-   #   if (Temp1==1) {
-   #     if (Temp3!=0) {
-   #       Temp = taxi.est.y-Temp3
-   #       Temp = ifelse(Temp<0,0,Temp)
-   #       Temp = ifelse((Temp-Temp2)<0,Temp,Temp2)
-   #       Temp <<- Temp
-   #     } else {
-   #       Temp = taxi.est.y
-   #       Temp = ifelse((Temp-Temp2)<0,Temp,Temp2)
-   #       Temp <<- Temp
-   #     }
-   #   } else {
-   #     if (Temp3!=0) {
-   #       Temp = taxi.est.y-Temp3
-   #       Temp = ifelse(Temp<0,0,Temp)
-   #       Temp = Temp*Temp2
-   #       Temp <<- Temp
-   #     } else {
-   #       Temp = taxi.est.y
-   #       Temp = Temp*Temp2
-   #       Temp <<- Temp
-   #     }
-   #   }
-   #   
-   #   ggplot(Temp,aes(x=taxiFee))+geom_density()+ggtitle("Single Years Total Payout")
-   # })
-   # 
-   # output$text3 <- renderText({
-   #   s.sd = abs(as.numeric(input$carsmax)-as.numeric(input$carsmin))/2
-   #   s.mean = as.numeric(input$carsmean)
-   #   paste0("一年的補貼成本可能落在", quantile(Temp$taxiFee[order(Temp$taxiFee)],0.1)[1],"元到", quantile(Temp$taxiFee[order(Temp$taxiFee)],0.9)[1],"元之間。（90%信賴區間）")
-   # })
+   output$distPlot3 <- renderPlot({
+
+     s.sd = abs(as.numeric(input$carsmax)-as.numeric(input$carsmin))/2
+     s.mean = as.numeric(input$carsmean)
+     # s.cars = rnorm(n = 365,mean = s.mean,sd=s.sd)
+     # s.cars = ifelse(s.cars<0,0,s.cars)
+     
+     # selectInput("plan","補貼策略：",choices = c("固定金額","固定比例"),multiple = F,selected = "固定比例"),
+     # numericInput("money",
+     #              "補貼比例或金額：",value = 0.1),
+     # selectInput("selfpay","有最小自付額嗎？",choices = c("是","否"),multiple = F,selected = "是"),
+     # numericInput("selfpaymoney",
+     #              "最小自付額（元）：",value = 100)
+
+     Temp.plan = input$plan
+     Temp.money = input$money
+     Temp.self = input$selfpay
+     Temp.pay = input$selfpaymoney
+       
+     if (Temp.plan == "固定金額") {
+       
+       if (Temp.self=="否") {
+         Temp.pay = 0
+       } 
+       
+       Temp.y=data.frame()
+       for (t in 1:30) {
+         Temp = data.frame(cars=s.cars)
+         Temp$taxiFee=0
+         for ( i in 1:nrow(Temp)) {
+           Temp$taxiFee[i] = taxi.trip$fee[sample(nrow(taxi.trip),abs(Temp$cars[i]),replace=T)] %>% 
+           {.-Temp.pay} %>% 
+             ifelse(.<0,0,.) %>% 
+             ifelse(.<Temp.money,.,Temp.money) %>% 
+             sum()
+         }
+         Temp = data.frame(cars = sum(Temp$cars),taxiFee=sum(Temp$taxiFee))
+         Temp.y =rbind(Temp.y,Temp)
+         rm(Temp)
+       }
+       
+       Temp.y <<- Temp.y
+       
+       
+     } else {
+       
+       if (Temp.self=="否") {
+         Temp.pay = 1
+       } 
+       
+       Temp.y=data.frame()
+       for (t in 1:30) {
+         Temp = data.frame(cars=s.cars)
+         Temp$taxiFee=0
+         for ( i in 1:nrow(Temp)) {
+           Temp$taxiFee[i] = taxi.trip$fee[sample(nrow(taxi.trip),abs(Temp$cars[i]),replace=T)] %>% 
+           {.-Temp.pay} %>% 
+             ifelse(.<0,0,.) %>% 
+             {.*Temp.money} %>% 
+             sum()
+         }
+         Temp = data.frame(cars = sum(Temp$cars),taxiFee=sum(Temp$taxiFee))
+         Temp.y =rbind(Temp.y,Temp)
+         rm(Temp)
+       }
+       
+       Temp.y <<- Temp.y
+       
+     }
+
+     ggplot(Temp.y,aes(x=taxiFee))+geom_density()+ggtitle("Single Years Total Payout")
+   })
+
+   output$text3 <- renderText({
+     s.sd = abs(as.numeric(input$carsmax)-as.numeric(input$carsmin))/2
+     s.mean = as.numeric(input$carsmean)
+     paste0("一年的補貼成本可能落在", quantile(Temp.y$taxiFee[order(Temp.y$taxiFee)],0.1)[1],"元到", quantile(Temp.y$taxiFee[order(Temp.y$taxiFee)],0.9)[1],"元之間。（90%信賴區間）")
+   })
    
 }
 
